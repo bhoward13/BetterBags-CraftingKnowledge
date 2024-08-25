fd4676ec70981f5f7855ba77a159513b4c17d0f6
Brian Howard <brian.howard@medicalguardian.com>
2024-08-25 11:54:50 -0400

    added more crafting knowledge items

--- thewarwithinKnowledge.lua
+++ thewarwithinKnowledge.lua
@@ -10,6 +10,12 @@ local TWW_Knowledge = {
     226270, -- Sanctified Mortar and Pestle
     226272, -- Dark Apothecary's Vial
     226271, -- Nerubian Mixing Salts
+    224024, -- Theories of Bodily Transmutation, Chapter 8
+    222546, -- Algari Treatise on Alchemy
+    225235, -- Deepstone Crucible
+    228773, -- Algari Alchemist's Notebook
+    227409, -- Faded Alchemist's Research
+    224645, -- Jewel-Etched Alchemy Notes
     -- #Blacksmithing
     226277, -- Dornogal Hammer
     226276, -- Ancient Earthen Anvil
@@ -19,6 +25,10 @@ local TWW_Knowledge = {
     226280, -- Holy Flame Forge
     226283, -- Spiderling's Wire Brush
     226282, -- Nerubian Smith's Kit]
+    224038, -- Smithing After Saronite
+    225233, -- Dense Bladestone
+    224038, -- Smithing After Saronite
+    222554, -- Algari Treatise on Blacksmithing
     -- #Enchanting
     226285, -- Silver Dornogal Rod
     226284, -- Grinded Earthen Gem
@@ -28,6 +38,13 @@ local TWW_Knowledge = {
     226288, -- Essence of Holy Fire
     226291, -- Void Shard
     226290, -- Book of Dark Magic
+    224050, -- Web Sparkles: Pretty and Powerful
+    225230, -- Crystalline Repository
+    227667, -- Algari Enchanter's Folio
+    225231, -- Powdered Fulgurance
+    224652, -- Jewel-Etched Enchanting Notes
+    227659, -- Fleeting Arcane Manifestation
+    222550, -- Algari Treatise on Enchanting
     -- #Engineering
     226293, -- Dornogal Spectacles
     226292, -- Rock Engineer's Wrench
@@ -37,6 +54,9 @@ local TWW_Knowledge = {
     226296, -- Holy Firework Dud
     226298, -- Puppated Mechanical Spider
     226299, -- Emptied Venom Canister
+    224052, -- Clocks, Gears, Sprockets, and Legs
+    227412, -- Faded Engineer's Scribblings
+    224653, -- Machine-Learned Engineering Notes
     -- #Herbalism
     226301, -- Dornogal Gardening Scythe
     226300, -- Ancient Flower
@@ -46,6 +66,13 @@ local TWW_Knowledge = {
     226305, -- Arathi Herb Pruner
     226307, -- Tunneler's Shovel
     226306, -- Web-Entangled Lotus
+    224023, -- Herbal Embalming Techniques
+    222552, -- Algari Treatise on Herbalism
+    224835, -- Deepgrove Roots
+    224264, -- Deepgrove Petal
+    227415, -- Faded Herbalist's Notes
+    224656, -- Void-Lit Herbalism Notes
+    224817, -- Algari Herbalist's Notes
     -- #Inscription
     226308, -- Dornogal Scribe's Quill
     226309, -- Historian's Dip Pen
@@ -55,6 +82,9 @@ local TWW_Knowledge = {
     226312, -- Informant's Fountain Pen
     226314, -- Nerubian Texts
     226315, -- Venomancer's Ink Well
+    222548, -- Algari Treatise on Inscription
+    225227, -- Wax-Sealed Records
+    224053, -- Eight Views on Defense against Hostile Runes
     -- #Jewelcrafting
     226317, -- Earthen Gem Pliers
     226316, -- Gentle Jewel Hammer
@@ -64,6 +94,10 @@ local TWW_Knowledge = {
     226320, -- Arathi Sizing Gauges
     226323, -- Nerubian Bench Blocks
     226322, -- Ritual Caster's Crystal
+    227413, -- Faded Jeweler's Illustrations
+    225225, -- Deepstone Fragment
+    222551, -- Algari Treatise on Jewelcrafting
+    224054, -- Emergent Crystals of the Surface-Dwellers
     -- #Leatherworking
     226324, -- Earthen Lacing Tools
     226325, -- Dornogal Craftsman's Flat Knife
@@ -73,6 +107,11 @@ local TWW_Knowledge = {
     226328, -- Arathi Beveler Set
     226331, -- Curved Nerubian Skinning Knife
     226330, -- Nerubian Tanning Mallet
+    222549, -- Algari Treatise on Leatherworking
+    225223, -- Sturdy Nerubian Carapace
+    224007, -- Uses for Leftover Husks (How to Take Them Apart)
+    224056, -- Uses for Leftover Husks (After You Take Them Apart)
+    225222, -- Stone-Leather Swatch
     -- #Mining
     226333, -- Dornogal Chisel
     226332, -- Earthen Miner's Gavel
@@ -82,6 +121,13 @@ local TWW_Knowledge = {
     226336, -- Arathi Precision Drill
     226338, -- Heavy Spider Crusher
     226339, -- Nerubian Mining Supplies
+    224055, -- A Rocky Start
+    224583, -- Slab of Slate
+    224818, -- Algari Miner's Notes
+    227427, -- Exceptional Miner's Notes
+    224651, -- Machine-Learned Mining Notes
+    227416, -- Faded Miner's Notes
+    222553, -- Algari Treatise on Mining
     -- #Skinning
     226340, -- Dornogal Carving Knife
     226341, -- Earthen Worker's Beams
@@ -91,6 +137,10 @@ local TWW_Knowledge = {
     226344, -- Arathi Tanning Agent
     226347, -- Carapace Shiner
     226346, -- Nerubian's Slicking Iron
+    224780, -- Toughened Tempest Pelt
+    222649, -- Algari Treatise on Skinning
+    224807, -- Algari Skinner's Notes
+    224782, -- Razor Talon
     -- #Tailoring
     226348, -- Dornogal Seam Ripper
     226349, -- Earthen Tape Measure
@@ -100,6 +150,15 @@ local TWW_Knowledge = {
     226353, -- Royal Outfitter's Protractor
     226354, -- Nerubian Quilt
     226355, -- Nerubian's Pincushion
+    224036, -- And That's a Web-Wrap!
+    228739, -- Glimmer of Tailoring Knowledge
+    228738, -- Flicker of Tailoring Knowledge
+    228779, -- Algari Tailor's Notebook
+    225220, -- Chitin Needle
+    225221, -- Spool of Webweave
+    224648, -- Jewel-Etched Tailoring Notes
+    227410, -- Faded Tailor's Diagrams
+    222547, -- Algari Treatise on Tailoring
 }
 
 addon.TWW_Knowledge = TWW_Knowledge
